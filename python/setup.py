6addde8c712414ef0619719003dc50d60a7c159f
Steinar Foss <steinar@unicus.no>
2019-07-25 10:48:39 +0200

    setuptools executes from root python.

--- python/setup.py
+++ python/setup.py
@@ -7,6 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Sunbeam',
+    package_dir = {'': 'python'},
     packages=[
                 'sunbeam',
                 'sunbeam.tools',
