e211eccfc7dc32b17e6379effa27354e3fac69fe
Steinar Foss <steinar@unicus.no>
2019-07-25 14:54:33 +0200

    python tests: temp reduced to test_deck only.

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,41 @@
 from setuptools import setup, find_packages
 
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
 
 
-
+ext_modules = [
+    Extension(
+        'libsunbeam',
+        [
+         'src/eclipse_state.cpp',
+         'src/deck_keyword.cpp',
+         'src/deck.cpp',
+         'src/well.cpp',
+         'src/sunbeam_state.cpp',
+         'src/schedule.cpp',
+         'src/connection.cpp',
+         'src/eclipse_config.cpp',
+         'src/table_manager.cpp',
+         'src/eclipse_grid.cpp',
+         'src/group.cpp',
+         'src/group_tree.cpp',
+         'src/eclipse_3d_properties.cpp',
+         'src/parser.cpp',
+         'src/sunbeam.cpp',
+        ],
+        include_dirs=[
+            'src',
+            '/home/stf/ert/install/include',
+            '/home/stf/opm/opm-common',
+            '/home/stf/opm/opm-common/build/include',
+            'pybind11/include'
+        ],
+        language='c++'
+    ),
+]
 
 
 setup(
@@ -13,6 +46,7 @@ setup(
                 'sunbeam.tools',
                 'sunbeam.deck',
             ],
+    ext_modules=ext_modules,
     license='Open Source',
     zip_safe=False,
     tests_suite=['tests'],
