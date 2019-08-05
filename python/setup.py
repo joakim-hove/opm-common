4c506d0a1851d84841f416c74bf94d4f9b05b4fa
Steinar Foss <steinar@unicus.no>
2019-08-05 12:16:40 +0200

    setup.py: discontinued use of glob, all files listed instead.

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,24 @@ import os
 ext_modules = [
     Extension(
         'libsunbeam',
-        glob.glob(os.path.join('src_sunbeam', '*.cpp')),
+        #glob.glob(os.path.join('src_sunbeam', '*.cpp')),
+        [
+                'src_sunbeam/connection.cpp',
+                'src_sunbeam/deck.cpp',
+                'src_sunbeam/deck_keyword.cpp',
+                'src_sunbeam/eclipse_3d_properties.cpp',
+                'src_sunbeam/eclipse_config.cpp',
+                'src_sunbeam/eclipse_grid.cpp',
+                'src_sunbeam/eclipse_state.cpp',
+                'src_sunbeam/group.cpp',
+                'src_sunbeam/group_tree.cpp',
+                'src_sunbeam/parser.cpp',
+                'src_sunbeam/schedule.cpp',
+                'src_sunbeam/sunbeam_state.cpp',
+                'src_sunbeam/table_manager.cpp',
+                'src_sunbeam/well.cpp',
+                'src_sunbeam/sunbeam.cpp'
+        ],
         libraries=['opmcommon', 'boost_filesystem', 'boost_regex', 'ecl'],
         language='c++',
         undef_macros=["NDEBUG"],
